893e361184745a526d432cdbc05e1d2654e4e7d6
cheacheaza0066 <chea14chainat@gmail.com>
2020-08-17 04:24:40 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  float R,H,ANS;
+  printf("Enter R ");
+  scanf("%f",&R);
+  printf("Enter H ");
+  scanf("%f",&H);
+  ANS =(2*22/7*R)*H+2*(22/7*R*R);
+  printf("ANSWER IS %.2f",ANS);
+  return 0;
+}
